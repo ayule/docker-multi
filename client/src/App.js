82aee0b9befb5ad0a430a8197f2d534ef87667dd
ayulemd <140634306+ayulemd@users.noreply.github.com>
2023-07-26 15:07:35 -0600

    Test pull request workflow

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Modified via public pull request
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
